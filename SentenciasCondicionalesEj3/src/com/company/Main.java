b6262666efcc5eed3a20f83611c20399d0aaeda6
cc <colauttiadad@gmail.com>
2022-05-23 15:31:48 -0300

    Ejercicio 3 de los condicionales

---
+++ SentenciasCondicionalesEj3/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*Cree un programa para un colegio, en el que se pueda ingresar por consola
+	el nombre del cursante y tres notas entre el 1 a 10. Con esta info imprimir:
+	a. nombre del cursante b. promedio de las notas c. indicar el estado del cursante
+	promedio entre 6 y 8 aprobado, 1 y 5 desaprobado, 9 y 10 promocionado
+	 */
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del cursante");
+        String nombre = myScanner.nextLine();
+        System.out.println("Ingrese la primer nota");
+        int nota1 = myScanner.nextByte();
+        System.out.println("Ingrese la segunda nota");
+        int nota2 = myScanner.nextByte();
+        System.out.println("Ingrese la tercer nota");
+        int nota3 = myScanner.nextByte();
+        int promedio = (nota1 + nota2 + nota3) / 3;
+
+        if (promedio>=1 && promedio<=5){
+            System.out.println("Nombre: " + nombre + " Promedio: " + promedio + " Estado: desaprobado" );
+        } else if (promedio>=6 && promedio<=8) {
+            System.out.println("Nombre: " + nombre + " Promedio: " + promedio + " Estado: Aprobado" );
+        } else if (promedio>=9 && promedio<=10) {
+            System.out.println("Nombre: " + nombre + " Promedio: " + promedio + " Estado: Promocionado");
+        }else{
+            System.out.println("Por favor, verifique que las notas ingresadas se encuentren en el rango del 1 al 10");
+        };
+}
+}
