6ea659d3d7a6bdfdaaa615037e5c0df2144bfcbf
cc <colauttiadad@gmail.com>
2022-05-23 15:44:39 -0300

    Ejercicio 1 de iterativas

---
+++ SentenciasIterativasEj1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Consigna: imprimir 10 veces en consola el string "foo"
+        for (int i = 0; i <10; i++) {
+            String texto= "foo";
+            System.out.println(texto);
+        }
+        }
+    }
