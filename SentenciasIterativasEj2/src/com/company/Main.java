13a775c122c2dcfea41354e06adf40fa07e7fe0c
cc <colauttiadad@gmail.com>
2022-05-23 18:11:42 -0300

    Iterativas Ej 2

---
+++ SentenciasIterativasEj2/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// imprimir en consola los números del 1 al 10, uno por línea
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
