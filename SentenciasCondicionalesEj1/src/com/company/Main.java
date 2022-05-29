93012b29904a027b11654739192816be121af5d9
cc <colauttiadad@gmail.com>
2022-05-29 16:11:19 -0300

    Ej1 de condicionales convertido en método

--- SentenciasCondicionalesEj1/src/com/company/Main.java
+++ SentenciasCondicionalesEj1/src/com/company/Main.java
@@ -4,20 +4,35 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         // Consigna: crear un programa que pida dos números por consola y que diga cuál es el mayor
+        guessLargerThan();
+
+    }
+
+    private static void guessLargerThan() {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("¡Hola! dime dos números y te diré cuál es el más grande, quieres jugar? Escribe tu primer número");
-        int number1 = myScanner.nextInt();
+        System.out.println("¡Hola! dime dos números y te diré cuál es el más grande");
+        System.out.println("Escribe tu primer número");
+        double number1 = myScanner.nextDouble();
         System.out.println("Genial! ahora escribe tu segundo número");
-        int number2 = myScanner.nextInt();
+        double number2 = myScanner.nextDouble();
         if (number1 > number2) {
             System.out.println("Tu primer número ("+ number1+") es el más grande");
         } else if (number2 > number1) {
             System.out.println("Tu segundo número ("+number2+") es el más grande");
-
+        }else System.out.println("Tus números son iguales! Por favor, escribe dos números distintos entre sí");
+        System.out.println("¿Quieres jugar de nuevo?");
+        System.out.println("1. Seguir jugando");
+        System.out.println("2. Salir");
+        int answer = myScanner.nextInt();
+        if (answer==1){
+            guessLargerThan();
         }else{
-            System.out.println("Por favor, escribe dos números que sean distintos");
+            System.out.println("Gracias por Jugar!");
+            return;
+        }
     }
-}
+
 }
