79ac7cddc66f124775b65b35f162dedafefce3ff
cc <colauttiadad@gmail.com>
2022-05-30 13:36:16 -0300

    Ej 2 de iterativas pasado a método

--- SentenciasIterativasEj2/src/com/company/Main.java
+++ SentenciasIterativasEj2/src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// imprimir en consola los números del 1 al 10, uno por línea
+        oneTo10();
+    }
+
+    private static void oneTo10() {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
