4c3e1ad5ebd9d892395caf4221e143a1cf3c718b
cc <colauttiadad@gmail.com>
2022-06-15 17:09:27 -0300

    Se mejoró el ejercicio extra 5

--- Ejercitación extra 5/src/com/company/Main.java
+++ Ejercitación extra 5/src/com/company/Main.java
@@ -6,22 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Ingrese un monto en pesos : ");
-        double pesosAmount = myScanner.nextDouble();
-        System.out.println("Ingrese a qué moneda lo desea convertir : ");
-        System.out.println("0. Dólares");
-        System.out.println("1. Reales");
-        System.out.println("2. Euros");
-        int currency = myScanner.nextInt();
-        convertCurrency(pesosAmount, currency);
+        double pesosAmount;
+        int currency;
+        do {
+            System.out.println("Ingrese un monto en pesos : ");
+            pesosAmount = myScanner.nextDouble();
+            System.out.println("Ingrese a qué moneda lo desea convertir : ");
+            System.out.println("1. Dólares");
+            System.out.println("2. Reales");
+            System.out.println("3. Euros");
+            currency = myScanner.nextInt();
+        } while (convertCurrency(pesosAmount, --currency));
     }
 
-    private static void convertCurrency(double pesosAmount, int currency) {
+    private static boolean convertCurrency(double pesosAmount, int currency) {
         double [] currencyValues = {206, 44.02, 223.84*1.23};
+        if (currency < currencyValues.length+1 && currency > 0){
         String [] currencyNames = {"dolares", "reales", "euros"};
         double convertedAmount = pesosAmount / currencyValues[currency];
         convertedAmount = Math.round(convertedAmount * 100.0) /100.0;
         String currencyName = currencyNames [currency];
         System.out.println("El valor de "+ pesosAmount + " pesos argentinos es igual a "+ convertedAmount + " "+ currencyName);
+        return true;
+        }
+        System.out.println("La opción no es válida");
+        return false;
     }
 }
